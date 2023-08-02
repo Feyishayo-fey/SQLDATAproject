0e1bc12b09ca3f0ef8738f5a5d69fa2f3b062fc1
Cisco. A <dexterhack800@gmail.com>
2023-08-02 03:37:33 -0600

    Revert "My_Queries"

--- schema.sql
+++ schema.sql
@@ -1,17 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-/*CREATING DATABASE*/
-
-CREATE DATABASE vet_clinic
-/*CREATING TABLES AND INPUTING COLUMNS*/
-
 CREATE TABLE animals (
-    id INT ,
-    name VARCHAR ,
-    date_of_birth DATE ,
-    escape_attempts INT,
-    neutered BIT,
-    weight_kg DECIMAL,
+    name varchar(100)
 );
-
-
